c0e3292ef521a586f738f15d62d318d0c143f3ce
Smithy <96345069+Smitthyy@users.noreply.github.com>
2023-04-05 00:20:44 +0100

    Added webhook & change title

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Webhook = '' -- Images will be uploaded here
+Config.Webhook = 'https://discord.com/api/webhooks/1092931148398727208/6Ggdhe08jb8D9zLcejlWvkTIO_7Ot0azmxCcqJekcUsSLTOlLYqX0yPOnNs6-VMWd8lb' -- Images will be uploaded here
 Config.TestCommand = true -- Use this when testing /testmugshot
 
 Config.CustomMLO = true -- If you use a MLO use the options below to change the camera location. Otherwise it will use the default IPL for the mugshot location
@@ -13,7 +13,7 @@ Config.MugShotCamera = {
     r = {x = 0.0, y = 0.0, z = 358.04} -- To change the rotation use the z. Others are if you want rotation on other axis
 }
 
-Config.BoardHeader = "Los Santos Police Department" -- Header that appears on the board
+Config.BoardHeader = "Metropolitan Police" -- Header that appears on the board
 Config.WaitTime = 2000 -- Time before and after the photo is taken. Decreasing this value might result in some angles being skiped.
 Config.Photos = 4 -- Front, Back Side. Use 4 for both sides
 Config.CQCMDT = true -- If you use CQC MDT this will automatically send them to a players profile
