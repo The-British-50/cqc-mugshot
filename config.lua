ff64bff6dd09f4baec0445f3692e60f1d7e7b4c1
Smithy <96345069+Smitthyy@users.noreply.github.com>
2023-04-05 01:09:30 +0100

    Edited x/y/z for mugshot

--- config.lua
+++ config.lua
@@ -4,13 +4,13 @@ Config.Webhook = 'https://discord.com/api/webhooks/1092931148398727208/6Ggdhe08j
 Config.TestCommand = true -- Use this when testing /testmugshot
 
 Config.CustomMLO = true -- If you use a MLO use the options below to change the camera location. Otherwise it will use the default IPL for the mugshot location
-Config.MugshotLocation = vector3(472.92, -1011.49, 25.27) -- Location of the Suspect
-Config.MugshotSuspectHeading = 183.55 -- Direction Suspsect is facing
+Config.MugshotLocation = vector3(458.86, -998.17, 23.91) -- Location of the Suspect vector4(458.86, -998.17, 24.91, 269.52)
+Config.MugshotSuspectHeading = 269.52 -- Direction Suspsect is facing
 Config.MugShotCamera = {
-    x = 473.05,
-    y = -1012.56,
-    z = 26.90,
-    r = {x = 0.0, y = 0.0, z = 358.04} -- To change the rotation use the z. Others are if you want rotation on other axis
+    x = 460.29,
+    y = -998.03,
+    z = 24.91,
+    r = {x = 0.0, y = 0.0, z = 89.85} -- To change the rotation use the z. Others are if you want rotation on other axis vector4(460.37, -998.1, 24.91, 86.91)
 }
 
 Config.BoardHeader = "Metropolitan Police" -- Header that appears on the board
